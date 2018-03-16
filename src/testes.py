c4d0201f017c3a9e2cfe87cd1b1fbb50e1d0e04c
1800408 <36746344+1800408@users.noreply.github.com>
2018-03-16 20:59:27 -0300

    Update testes.py

--- src/testes.py
+++ src/testes.py
@@ -1,4 +1,4 @@
-Import jogovelha
+import jogovelha
 import sys
 
 erroInicializar = False
@@ -16,5 +16,5 @@ if len(jogo) != 3:
             erroInicializar = True
 if erroInicializar:
   sys.exit(1)
-  else:
-    sys.exit(0)
+else:
+  sys.exit(0)
